8d50df6c66f9fde0d14c84d3fb0af0b38d51d51b
StaCKm29 <mamartinez2023@udec.cl>
2024-05-23 22:14:06 -0400

    Creacion de clases para elementos graficos

--- src/main/java/org/modelos/Expendedor.java
+++ src/main/java/org/modelos/Expendedor.java
@@ -1,4 +1,14 @@
 package org.modelos;
 
 public class Expendedor {
+    public Producto comprarProducto (int pago, Selector eleccion) throws NoHayProductoException, PagoInsuficienteException{
+        int i = eleccion.ordinal();
+        if(pago < eleccion.getprecio()){
+            throw new PagoInsuficienteException("El pago es insuficiente.");
+        }
+        if(depositos[eleccion.ordinal()].isEmpty()){
+            throw new NoHayProductoException("No hay producto." + eleccion.toString().toLowerCase());
+        }
+        return depositos[i].remove(depositos[i].size()-1);
+    }
 }
--- src/main/java/org/modelos/Selector.java
+++ src/main/java/org/modelos/Selector.java
@@ -1,4 +1,9 @@
 package org.modelos;
 
 public enum Selector {
+    COCACOLA(1200),
+    FANTA(1200),
+    SPRITE(1200),
+    SNICKERS(1850),
+    SUPER8(300);
 }
