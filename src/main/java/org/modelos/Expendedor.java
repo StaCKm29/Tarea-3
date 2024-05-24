ec460fe62f0da744a3352e473289396082c94dec
zMRGABOz <gacastillo2023@udec.cl>
2024-05-24 00:20:21 -0400

    Modificacion de Expendedor y adicion de PanelPrincipal

--- src/main/java/org/modelos/Expendedor.java
+++ src/main/java/org/modelos/Expendedor.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class Expendedor {
     private ArrayList<Producto>[] depositos;
     private Deposito<Moneda> monedaVuelto;
+    private Producto producto;
+    private Deposito<Moneda> monedasPago;
 
     public Expendedor(int size){
         depositos = new ArrayList[Selector.values().length];
@@ -17,7 +19,7 @@ public class Expendedor {
         }
     }
 
-    public Producto comprarProducto (Moneda pago, Selector eleccion) throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException{
+    public void comprarProducto (Moneda pago, Selector eleccion) throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException{
         int i = eleccion.ordinal();
         if(pago == null){
             throw new PagoIncorrectoException("El pago es incorrecto.");
@@ -28,7 +30,12 @@ public class Expendedor {
         if(depositos[eleccion.ordinal()].isEmpty()){
             throw new NoHayProductoException("No hay producto." + eleccion.toString().toLowerCase());
         }
-        return depositos[i].remove(depositos[i].size()-1);
+        monedasPago.addObjeto(pago);
+        producto = depositos[i].remove(depositos[i].size()-1);
+    }
+
+    public Producto getProducto(){
+        return producto;
     }
 
     public Moneda getVuelto() {
--- src/main/java/org/vistas/PanelPrincipal.java
+++ src/main/java/org/vistas/PanelPrincipal.java
@@ -1,4 +1,18 @@
 package org.vistas;
+import javax.swing.JPanel;
+import java.awt.*;
 
-public class PanelPrincipal {
+public class PanelPrincipal extends JPanel {//se ve en el centro de la ventana
+    private PanelComprador com;
+    private PanelExpendedor exp;
+    public PanelPrincipal () {
+        exp = new PanelExpendedor (..);
+        com = new PanelComprador(..);
+        this.setBackground(Color.white);
+    }
+    public void paintComponent (Graphics g) {
+        super.paintComponent(g);
+        com.paintComponent(g);
+        exp.paintComponent(g);
+    }
 }
