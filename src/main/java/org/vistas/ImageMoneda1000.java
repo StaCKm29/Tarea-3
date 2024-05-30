c151e2e6a7bd31ed130d22bb6e30cf655e83078c
zMRGABOz <gacastillo2023@udec.cl>
2024-05-29 20:36:35 -0400

    Adicion de clases ImageMonedas

---
+++ src/main/java/org/vistas/ImageMoneda1000.java
@@ -0,0 +1,31 @@
+package org.vistas;
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
+
+public class ImageMoneda1000 extends ImageMonedas{
+    private int valor;
+    private String ruta;
+    private Image imagen;
+    public ImageMoneda1000() {
+        super();
+        this.ruta = "moneda1000.png";
+        this.valor = 1000;
+        URL imagenURL = getClass().getClassLoader().getResource(ruta);
+        try{
+            imagen = new ImageIcon(imagenURL).getImage();
+            int ancho = 70;
+            int alto = 70;
+            imagen = imagen.getScaledInstance(ancho, alto, Image.SCALE_DEFAULT);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    int getValor() {
+        return this.valor;
+    }
+}
